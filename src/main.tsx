52b7a9984d1bbaa72ca33eda98cb3eff54a365b3
Gaspi <gasparelpi@gmail.com>
2022-09-03 21:37:07 -0300

    changing hash to browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ChakraProvider theme={theme}>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
       </ChakraProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
